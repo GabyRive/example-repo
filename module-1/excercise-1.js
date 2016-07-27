363f1d035da5314a81f6f736bf24fbd69cebf1dc
GabyRive <gabrielarivera@Gabrielas-MacBook.local>
2016-07-26 21:13:29 -0400

    Codetrotters exercises intro to javascript

---
+++ module-1/excercise-1.js
@@ -0,0 +1,25 @@
+//number
+console.log(1);
+//string
+console.log('hello');
+//variable
+var x =1;
+console.log(x);
+//expressions
+console.log(x + 2);
+//multiple arguments
+console.log(x, x, 1, 2 +2);
+//MATH EXERCISES
+// + example#1
+console.log(1 +1);
+//+ example#2
+var a = 3
+var b = 5
+console.log(a + b);
+//post and pre increment example
+var x = 2
+var y = 3
+console.log(x++);
+console.log(y++);
+console.log(++x);
+console.log(++y);
