b960c879610a244a939b74c0e560cd15b34c23b2
GabyRive <gabrielarivera@GabrielsMacBook.lan>
2016-07-30 22:35:19 -0400

    Exercises from module 1. Exercise 5 stuck

--- module-1/excercise-1.js
+++ module-1/excercise-1.js
@@ -1,3 +1,4 @@
+//Exercise 1
 //number
 console.log(1);
 //string
@@ -16,6 +17,7 @@ console.log(1 +1);
 var a = 3
 var b = 5
 console.log(a + b);
+//Exercise 2
 //post and pre increment example
 var x = 2
 var y = 3
@@ -23,3 +25,23 @@ console.log(x++);
 console.log(y++);
 console.log(++x);
 console.log(++y);
+//Exercise 3
+var prompt = require('prompt-sync')();
+var x = Number(prompt('What you want as your first number?'));
+var y = Number(prompt('What you want as your second number?'));
+console.log(x - y);
+//Exrcise 4
+var prompt = require('prompt-sync')();
+var x = Number(prompt('What you want as your number?'));
+console.log(x % 2);
+//Exercise 5 (stuck)
+var prompt = require('prompt-sync')();
+var x = Number(prompt('What you want as your first number?'));
+var y = Number(prompt('What you want as your second number?'));
+console.log(x/y, y/x);
+//Exercise 6
+var prompt = require('prompt-sync')();
+var A = Number(prompt('How many ounces of Medalla did you had?'));
+var W = Number(prompt('Enter your weight?'));
+var H = Number(prompt('How many hours has passed since your last Medalla?'));
+console.log((A * 5.14/W * 0.69) - 0.015 * H );
