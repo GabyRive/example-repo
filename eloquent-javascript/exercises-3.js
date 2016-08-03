aeaded3b47655a4fba2e2e8d88ddf3b1ff99d3aa
GabyRive <gabrielarivera@GabrielsMacBook.lan>
2016-08-03 09:11:14 -0400

    renamed directory and added new directory

---
+++ eloquent-javascript/exercises-3.js
@@ -0,0 +1,12 @@
+//Examples
+//Methods
+var mack = [];
+mack.push("Mack");
+mack.push("the", "Knife");
+//.push - method can be used to add values to the end of an array. 
+console.log(mack);
+console.log(mack.join(" "));
+//.join - determines the text that is glued between the array’s elements.
+console.log(mack.pop());
+//.pop - it removes the value at the end of the array and returns it.
+console.log(mack);
